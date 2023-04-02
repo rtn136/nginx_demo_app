2e76957cbbbdb9a976d7709c260a9f5a3b227035
Ubuntu <ubuntu@ip-172-31-3-240.ec2.internal>
2023-04-02 06:14:57 +0000

    added echo statements for better instructions in docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+
+echo "Removing existing image..."
 docker rmi nginx_app_img
+echo "Buidling new image..."
 docker build -t nginx_app_img .
+echo "Removing existing container..."
 docker rm $(docker stop nginx_app_cont)
+echo "Running new container..."
 docker run -itd --name nginx_app_cont -p 8090:80 nginx_app_img
 echo "Image built and container is running at $(curl http://checkip.amazonaws.com):8090"
+
+
