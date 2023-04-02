d5b0a1e20604c34513b8ca2b875dd8f926c22b49
rtn136 <rtn136@gmail.com>
2023-04-02 12:11:11 +0530

    beautified instructions on docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-echo "Removing existing image..."
+echo "===================================Removing existing image============================================="
 docker rmi nginx_app_img
-echo "Buidling new image..."
+echo "===================================Buidling new image============================================="
 docker build -t nginx_app_img .
-echo "Removing existing container..."
+echo "===================================Removing existing container============================================="
 docker rm $(docker stop nginx_app_cont)
-echo "Running new container..."
+echo "===================================Running new container============================================="
 docker run -itd --name nginx_app_cont -p 8090:80 nginx_app_img
-echo "Image built and container should be running at $(curl http://checkip.amazonaws.com):8090"
+echo -e "\n\nImage built and container should be running at $(curl http://checkip.amazonaws.com):8090"
