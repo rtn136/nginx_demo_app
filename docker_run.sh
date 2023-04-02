e6838897ecdfe0a22ea82a3194e20ad2da22c07c
rtn136 <rtn136@gmail.com>
2023-04-02 12:25:31 +0530

    added force remove

--- docker_run.sh
+++ docker_run.sh
@@ -2,11 +2,11 @@
 
 
 echo "===================================Removing existing image============================================="
-docker rmi nginx_app_img
+docker rmi -f nginx_app_img
 echo "===================================Buidling new image============================================="
 docker build -t nginx_app_img .
 echo "===================================Removing existing container============================================="
-docker rm $(docker stop nginx_app_cont)
+docker rm -f $(docker stop nginx_app_cont)
 echo "===================================Running new container============================================="
 docker run -itd --name nginx_app_cont -p 8090:80 nginx_app_img
 echo -e "\n\nImage built and container should be running at $(curl http://checkip.amazonaws.com):8090"
