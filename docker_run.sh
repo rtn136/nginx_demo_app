0d31de927d3da8eb647d4018d9318a4c30824da9
rtn136 <rtn136@gmail.com>
2023-04-03 15:51:40 +0530

    -s in docker_run

--- docker_run.sh
+++ docker_run.sh
@@ -18,7 +18,7 @@ if [ $run_cont == "y" ] || [ $run_cont == "Y" ];then
     docker rm -f $(docker stop nginx_app_cont)
     echo "===================================Running new container==============================================="
     docker run -itd --name nginx_app_cont -p 8090:80 nginx_app_img
-    echo -e "\n\nImage built and container should be running at http://$(curl http://checkip.amazonaws.com):8090\n\n"
+    echo -e "\n\nImage built and container should be running at \n\nhttp://$(curl -s http://checkip.amazonaws.com):8090\nhttp://$(curl -s http://checkip.amazonaws.com):8090/solution\nhttp://$(curl -s http://checkip.amazonaws.com):8090/fashion\n\n"
 
 elif [ $run_cont == "n" ] || [ $run_cont == "N" ];then
     echo -e "\n\nImage built and pushed into registry. You have chosen not to run the container.\n\n"
