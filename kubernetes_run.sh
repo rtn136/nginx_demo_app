90bd9a595ef0b359fe794f96530a8127635407e9
rtn136 <rtn136@gmail.com>
2023-04-03 15:47:04 +0530

    ;

--- kubernetes_run.sh
+++ kubernetes_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "===================================Applying configurations============================================="
-kubectl apply -f nginx_k8s_app.yml
+kubectl apply -f nginx_k8s_app.yml;
 echo -e "\n\nApplication running... Try: \nhttp://$(curl http://checkip.amazonaws.com):31000\nhttp://$(curl http://checkip.amazonaws.com):31000/solution\nhttp://$(curl http://checkip.amazonaws.com):31000/fashion\n\n"
