0d0b4619b7e1aa462604eddd708db01e64649d18
Ubuntu <ubuntu@ip-172-31-21-33.ec2.internal>
2023-04-03 10:11:09 +0000

    changed permission kubernetes.sh

--- kubernetes_run.sh
+++ kubernetes_run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo "===================================Applying configurations============================================="
 kubectl apply -f nginx_k8s_app.yml
 echo -e "\n\nApplication running at http://$(curl http://checkip.amazonaws.com):31000\n\n"
